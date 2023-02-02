17d50916857b5868d58d07088cbe48722d8c68ad
JoshAltman <joshuaealtman@gmail.com>
2023-02-01 21:10:52 -0500

    getting player character to work

--- Source/GlobalGameJam2023/RooterShooterPawn.h
+++ Source/GlobalGameJam2023/RooterShooterPawn.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
+#include "Components/CapsuleComponent.h"
 #include "RooterShooterPawn.generated.h"
 
 UCLASS()
@@ -15,9 +17,45 @@ public:
 	// Sets default values for this pawn's properties
 	ARooterShooterPawn();
 
+	UPROPERTY(EditAnywhere)
+	class UCapsuleComponent* Capsule;
+
+	UPROPERTY(EditAnywhere)
+	class UStaticMeshComponent* StaticMesh;
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* LookAction;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
 
 public:	
 	// Called every frame
